4c5f7185e38a99e2bc920710916c4b7f186aa05a
peter-schweitzer <peter.schweitzer.official@gmail.com>
2023-12-01 13:38:37 +0100

    reorganize types.js

--- src/types.js
+++ src/types.js
@@ -1,15 +1,25 @@
 /**
- * @typedef {{err: T, data: null}} SpecificErr<T>
- * @template {string} T
- */
-
-/**
- * @typedef {SpecificErr<string>} Err
- */
-
-/**
  * @typedef {{err: null, data: T}} Data<T>
  * @template T
+ */
+
+/**
+ * @typedef {{err: string, data: null}} Err
+ */
+
+/**
+ * @typedef {Err | Data<T>} ErrorOr<T>
+ * @template T
+ */
+
+/**
+ * @typedef {Promise<ErrorOr<T>>} AsyncErrorOr<T> The Promise should always resolves, never reject!
+ * @template T
+ */
+
+/**
+ * @typedef {{err: T, data: null}} SpecificErr<T>
+ * @template {string} T
  */
 
 /**
@@ -22,16 +32,6 @@
  * @typedef {Promise<SpecificErrorOr<T,K>>} AsyncSpecificErrorOr<T,K> The Promise should always resolves, never reject!
  * @template {string} T
  * @template K
- */
-
-/**
- * @typedef {SpecificErrorOr<string, T>} ErrorOr<T>
- * @template T
- */
-
-/**
- * @typedef {AsyncSpecificErrorOr<string,T>} AsyncErrorOr<T> The Promise should always resolves, never reject!
- * @template T
  */
 
 /**

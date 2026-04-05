76adf2a3f7b698276469209e13be7196969d8aec
peter-schweitzer <peter.schweitzer.official@gmail.com>
2026-04-05 17:00:37 +0200

    update module resolution to nodenext
    this is necessary to be able to use typescript 6
    this also improves DX by changing the type export mechanism
    types are now defined in `src/types.d.ts`
    types are a lot nicer to write in ts, rather than JSDoc annotations

---
+++ src/types.d.ts
@@ -0,0 +1,29 @@
+export declare global {
+  type Data<T> = { err: null; data: T };
+
+  type Err = { err: string; data: null };
+  type ErrorOr<T> = Err | Data<T>;
+  /** The Promise should always resolves, never reject! */
+  type AsyncErrorOr<T> = Promise<ErrorOr<T>>;
+
+  type SpecificErr<T extends string> = { err: T; data: null };
+  type SpecificErrorOr<T extends string, K> = SpecificErr<T> | Data<K>;
+  /** The Promise should always resolves, never reject! */
+  type AsyncSpecificErrorOr<T extends string, K> = Promise<SpecificErrorOr<T, K>>;
+
+  type FalseOr<T> = false | T;
+  type LUT<T> = { [key: string]: T };
+
+  type Schema = { [key: string]: 'boolean' | 'number' | 'string' | 'object' | 'array' | 'null' | 'any' | Schema };
+  type Schema_T<S extends Schems> = {
+    [K in keyof S]: S[K] extends 'boolean' ? boolean
+    : S[K] extends 'number' ? number
+    : S[K] extends 'string' ? string
+    : S[K] extends 'object' ? Object
+    : S[K] extends 'array' ? any[]
+    : S[K] extends 'null' ? null
+    : S[K] extends 'any' ? any
+    : S[K] extends Schema ? Schema_T<S[K]>
+    : never;
+  };
+}
